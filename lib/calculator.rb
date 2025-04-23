e0e5e54448abc27f02ed829927488cad3f9b0a8a
atiquevellore <atiquevellore@gmail.com>
2025-04-23 15:53:41 +0530

    sixth scenario handled the custom delimeters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,18 +1,25 @@
 class Calculator
     def self.add(numbers)
+        sum = 0
+        delimiter_pattern = %r{//(.+)\n}
+
         # Check if the input is ""
         if numbers.empty?
             return 0
         end
-        if numbers.include?("\n")
-            numbers.gsub!("\n", ",")
+
+        
+        if numbers.match?(delimiter_pattern)
+            # Extracts  the custom delimiter and escape it to handle special regex characters like ., *, |
+            delimiter = Regexp.escape(numbers.match(delimiter_pattern)[1])
+            numbers = numbers.sub(delimiter_pattern, '')             # Remove the delimiter declaration part (e.g., "//;\n")
+            numbers = numbers.gsub(/#{delimiter}/, ',')              # Replace all occurrences of the custom delimiter with commas
         end
         numbers = numbers.split(",")
         # Check if the input is a single number
         if numbers.length == 1
             return numbers[0].to_i
         end
-        sum = 0 
         numbers.each { |number|  sum+= number.to_i }
         return sum
     end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -28,5 +28,9 @@ RSpec.describe Calculator do
         it "Expecting method to handle new line as delimiter" do
             expect(Calculator.add("1\n2,3")).to eq(6)
         end
+
+        it "Expecting method to handle custom delimiter" do
+            expect(Calculator.add("//;\n1;2")).to eq(3)
+        end
     end
 end
